f368691dd500d330f8f7c20d3f3ac8b0bfe14854
Cihan <work@Cihans-MacBook-Pro.local>
2018-01-26 02:14:03 +0300

    Sidebar component is now stateless instead of stateful

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,26 +2,14 @@ import React from 'react';
 import Members from './Members';
 import ProjectInfo from './ProjectInfo';
 
-
-class Sidebar extends React.Component {
-    constructor(props){
-        super(props);
+const Sidebar = (props) => {
+    return(
+        <div className="sidebar-container">
+            <ProjectInfo projectName={props.projectName} projectType={props.projectType} projectDensity={props.projectDensity} />
+            <Members memberInfo={props.members} />
+        </div>                   
         
-    }
-    
-    render() {
-        return(
-            <div className="sidebar-container">
-                <ProjectInfo projectName={this.props.projectName} projectType={this.props.projectType} projectDensity={this.props.projectDensity} />
-                <Members memberInfo={this.props.members} />
-            </div>                   
-            
-        )
-        
-    }
-
-
-    
+    )
 }
 
 
